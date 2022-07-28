0d536fc06540b5e65b17cbc9e2e32a0a9eaddcb7
Eric Toreki <etoreki@udel.edu>
2022-07-28 14:40:27 -0400

    Initial header with editing being implemented

--- src/DisplaySet.tsx
+++ src/DisplaySet.tsx
@@ -1,5 +1,6 @@
 import { DiceSet } from "./Interfaces/diceSet";
-import React from "react";
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export function DisplaySet({
     diceSets,
@@ -10,5 +11,65 @@ export function DisplaySet({
     setDiceSets: (newDiceSets: DiceSet[]) => void;
     currentSet: DiceSet;
 }): JSX.Element {
-    return <div></div>;
+    const [edit, setEdit] = useState<boolean>(false);
+    const [name, setName] = useState<string>(currentSet.name);
+    const [primColor, setPrimColor] = useState<number>(currentSet.primaryColor);
+    const [secondColor, setSecondColor] = useState<number>(
+        currentSet.secondaryColor
+    );
+    function editName(event: React.ChangeEvent<HTMLInputElement>) {
+        setName(event.target.value);
+    }
+    function saveChanges() {
+        const newSet = {
+            ...currentSet,
+            name: name,
+            primaryColor: primColor,
+            secondaryColor: secondColor
+        };
+        const newDiceSets = diceSets.map(
+            (set: DiceSet): DiceSet =>
+                set.id === currentSet.id ? newSet : { ...set }
+        );
+        setDiceSets(newDiceSets);
+        setEdit(false);
+    }
+    function cancelChanges() {
+        setName(currentSet.name);
+        setPrimColor(currentSet.primaryColor);
+        setSecondColor(currentSet.secondaryColor);
+        setEdit(false);
+    }
+    return (
+        <div>
+            <table className="Table-Header"></table>
+            {edit ? (
+                <div>
+                    <Form.Group className="dropdownWidth" controlId="setName">
+                        <Form.Label>Name of Dice Set: </Form.Label>
+                        <Form.Control value={name} onChange={editName} />
+                    </Form.Group>
+                    <Button onClick={saveChanges}>Save Changes</Button>
+                    <Button onClick={cancelChanges}>Cancel</Button>
+                </div>
+            ) : (
+                <div>
+                    <h4>
+                        {semester.name}{" "}
+                        <Button
+                            className="btntransparent"
+                            onClick={() => setEdit(true)}
+                        >
+                            <img
+                                src="https://cdn-icons-png.flaticon.com/512/84/84380.png"
+                                height="40"
+                                width="40"
+                            />
+                            Edit Semester Name
+                        </Button>
+                    </h4>
+                </div>
+            )}
+        </div>
+    );
 }
