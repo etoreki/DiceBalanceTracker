e2daa93224b34d450fceae95260b8ddd88fc0406
Eric Toreki <etoreki@udel.edu>
2022-07-27 21:36:38 -0400

    Basic Info

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,10 @@ import "./App.css";
 function App(): JSX.Element {
     return (
         <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
+            <header className="App-header">Dice Balance Checker</header>
             <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
+                Based on number of rolls of each face of a die determines if the
+                die is balanced.
             </p>
         </div>
     );
