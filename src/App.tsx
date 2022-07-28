dbb06b8d8c5653d0d0233c1dd69494fefeabdf04
Eric Toreki <etoreki@udel.edu>
2022-07-27 23:28:56 -0400

    Setting up local save data

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
+let data: DiceSet[] | (() => DiceSet[]) = [];
+const saveDataKey = "DICE-BALANCE-TRACKER-DATA";
+// Check if the user's data already exists
+const previousData = localStorage.getItem(saveDataKey);
+// If the data doesn't exist, `getItem` returns null
+if (previousData !== null) {
+    data = JSON.parse(previousData);
+}
+
 function App(): JSX.Element {
+    const [diceSets, setDiceSets] = useState<DiceSet[]>(data);
     return (
         <div className="App">
             <header className="App-header">Dice Balance Checker</header>
